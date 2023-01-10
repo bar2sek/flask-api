a798ebaa10770733e60139b390050ddc4b2689c9
Ryan Bartusek <bar2sek@outlook.com>
2023-01-10 15:18:15 -0600

    WIP: add delete and test

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import flask_restful
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
 
@@ -11,14 +12,30 @@ video_put_args.add_argument("likes", type=int, help="Likes on video")
 
 videos = {}
 
+def abort_if_video_if_doesnt_exist(video_id):
+    if video_id not in videos:
+        flask_restful.abort(404, message="video not found")
+
+def abort_if_video_exists(video_id):
+    if video_id in videos:
+        flask_restful.abort(409, message="video already exists")
+
 class Video(Resource):
     def get(self, video_id):
+        abort_if_video_if_doesnt_exist(video_id)
         return videos[video_id]
 
     def put(self, video_id):
+        abort_if_video_exists(video_id)
         args = video_put_args.parse_args() # will get all args from uri input
         videos[video_id] = args
         return videos[video_id], 201
+
+    def delete(self, video_id):
+        abort_if_video_if_doesnt_exist(video_id)
+        del videos[video_id]
+        return '', 204
+
 
 api.add_resource(Video, "/video/<int:video_id>") # registering the resource to go to this class at this uri
 
--- test.py
+++ test.py
@@ -2,8 +2,21 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "video/1", {"likes": 10, "name": "Ryan", "views": 20})
-print(response.json())
+data = [{"likes": 10, "name": "Wow! What a dumb video!", "views": 20},
+        {"likes": 45, "name": "How to program", "views": 500},
+        {"likes": 123420, "name": "Why python is cool", "views": 3231231234},
+        {"likes": 2345, "name": "Shoveling snow", "views": 3211231}]
+
+for i in range(len(data)):
+    response = requests.put(BASE + f"video/{i}", data[i])
+    print(response.json())
+
 input()
-response = requests.get(BASE + "video/1")
+
+response = requests.delete(BASE + "video/0")
+print(response)
+
+input()
+
+response = requests.get(BASE + "video/2")
 print(response.json())
