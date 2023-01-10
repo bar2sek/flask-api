fa4fa4b34a5424161109e9eb2bc7736a4990c8b1
Ryan Bartusek <bar2sek@outlook.com>
2023-01-10 16:14:22 -0600

    WIP: add database

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import flask_restful
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, abort
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class VideoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    views = db.Column(db.Integer, nullable=False)
+    likes = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Video(name={name}, views={views}, likes={likes})"
+
+# db.create_all()
 
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type=str, help="Name of video is required", required=True)
@@ -14,11 +28,11 @@ videos = {}
 
 def abort_if_video_if_doesnt_exist(video_id):
     if video_id not in videos:
-        flask_restful.abort(404, message="video not found")
+        abort(404, message="video not found")
 
 def abort_if_video_exists(video_id):
     if video_id in videos:
-        flask_restful.abort(409, message="video already exists")
+        abort(409, message="video already exists")
 
 class Video(Resource):
     def get(self, video_id):
